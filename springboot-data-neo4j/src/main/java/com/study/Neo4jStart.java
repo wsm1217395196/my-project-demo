7621a6b4b7c2c91d2b56ec950ba9f161d4100753
wsm <1217395196@qq.com>
2019-07-11 11:13:45 +0800

    新增springboot-data-neo4j项目。 新增template模板项目。

---
+++ springboot-data-neo4j/src/main/java/com/study/Neo4jStart.java
@@ -0,0 +1,14 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jStart.class, args);
+    }
+
+}
+
