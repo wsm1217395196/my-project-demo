a0adce79aee529b61e88c47495bf4fc01d561130
wsm <1217395196@qq.com>
2020-09-05 11:34:41 +0800

    新增springboot-webSocket项目

---
+++ springboot-webSocket/src/main/java/com/study/result/PageParam.java
@@ -0,0 +1,68 @@
+package com.study.result;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 分页参数类
+ */
+@Getter
+@Setter
+@ToString
+@ApiModel(value = "PageParam对象", description = "分页参数类")
+public class PageParam<T> {
+    /**
+     * 要查第几页？
+     */
+    @ApiModelProperty(value = "要查第几页？")
+    private int pageIndex;
+    /**
+     * 要查一页多少数据？
+     */
+    @ApiModelProperty(value = "要查一页多少数据？")
+    private int pageSize;
+    /**
+     * 要查的字段（注意：别名对应model属性名）
+     */
+    @ApiModelProperty(value = "要查的字段（注意：别名对应model属性名）")
+    private String sqlColumns;
+    /**
+     * 排序字段（如 update_time-desc,name-asc）
+     */
+    @ApiModelProperty(value = "排序字段（如 update_time-desc,name-asc）")
+    private String sort;
+    /**
+     * 条件(json格式："{'name':'wsm','isEnable':'1'}")
+     */
+    @ApiModelProperty(value = "条件(json格式：\"{'name':'wsm','isEnable':'1'}\")")
+    private String condition;
+
+    /**
+     * 条件
+     */
+    @ApiModelProperty(value = "条件")
+    private T objectCondition;
+
+    public PageParam() {
+    }
+
+    public PageParam(int pageIndex, int pageSize, String sqlColumns, String sort, String condition, T objectCondition) {
+        this.pageIndex = pageIndex;
+        this.pageSize = pageSize;
+        this.sqlColumns = sqlColumns;
+        this.sort = sort;
+        this.condition = condition;
+        this.objectCondition = objectCondition;
+    }
+
+    public int getPageStart() {
+        if (pageIndex > 0) {
+            return (pageIndex - 1) * pageSize;
+        }
+        return pageIndex;
+    }
+
+}
